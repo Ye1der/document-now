d71f329a0cae383e58eb2f09f26cc7baedf5713f
Yeider <yeiderjp12@gmail.com>
2024-07-10 14:02:26 -0500

    firts commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <section>
+      <Button>Document Now!</Button>
+    </section>
+  );
+}
+
+export default App;
